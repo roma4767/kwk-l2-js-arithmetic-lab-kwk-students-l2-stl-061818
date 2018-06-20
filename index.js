64002acaf1a0774836dffd498fd77296474b60c7
Romina D-R <rdr2711@yahoo.com>
2018-06-20 20:44:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-let newID = 1000000000 + oldID
+let newID = 1000000000 + oldID;
 
-const ageIsValid = 
+const ageIsValid = Math.round(currentAge);
+
